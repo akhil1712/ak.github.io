d9dc5500aa41561ba80f2b795812470c3790cbed
Akhil <67535568+akhil1712@users.noreply.github.com>
2021-02-03 22:23:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function toggle(){
+    var sec = document.getElementById('sec');
+    var activeToggle = document.getElementById('toggle');
+
+    sec.classList.toggle('active');
+    activeToggle.classList.toggle('active');
+}
